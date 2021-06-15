8dc180f75ac3d474fd66723907e1fb9302957a84
Florent POLLET <ap.ap@club-internet.fr>
2021-06-15 13:15:44 +0200

    Updates

---
+++ precache-manifest.ececa3638be8672cf27169b10849124b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcbf6c8cb828fb08f6a542794122c2f",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "86ca8e29847dee7d5765",
+    "url": "/blindtest-spotify/static/css/main.348ad1c0.chunk.css"
+  },
+  {
+    "revision": "99291f00ffc51cf5bd8a",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ca8e29847dee7d5765",
+    "url": "/blindtest-spotify/static/js/main.38917143.chunk.js"
+  },
+  {
+    "revision": "01c4d54207b3c1322762",
+    "url": "/blindtest-spotify/static/js/runtime-main.e8875b69.js"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blindtest-spotify/precache-manifest.af1d10f296fb8bac425bec0e56195ae1.js"
+  "/blindtest-spotify/precache-manifest.ececa3638be8672cf27169b10849124b.js"
 );
 
 self.addEventListener('message', (event) => {
